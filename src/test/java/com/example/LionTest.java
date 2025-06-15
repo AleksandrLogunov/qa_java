a55ffb9d359fd8a7e78eb7459f138c38ee78dc71
Aleksandr Logunov <alogunov@turboapp.ru>
2025-06-15 09:23:34 +0300

    Первая отправка проекта на ревью

--- src/test/java/com/example/LionTest.java
+++ src/test/java/com/example/LionTest.java
@@ -2,14 +2,11 @@ package com.example;
 
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.Mockito;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
 
 public class LionTest {
 
